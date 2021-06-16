6e14e75c1811f288cda4e6fba63047a4fea9a410
hochan Lee <hochan049@gmail.com>
2021-06-16 21:38:10 +0900

    chore: add storybook

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,7 @@
-import { configure } from '@storybook/react';
-const req = require.context('../src', true, /\.stories\.tsx$/);
+import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
+const req = requireContext("../src", true, /\.stories\.tsx$/);
 function loadStories() {
-  req.keys().forEach(req);
+    req.keys().forEach(req);
 }
 configure(loadStories, module);
